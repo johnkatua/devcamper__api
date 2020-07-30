f0a95797e8ac40243b54ae07997f2a4a979cadc5
john katua <johnkatua99@gmail.com>
2020-07-31 01:51:58 +0300

    added seeder which enables to destruct our data and import to mongoDB compass, we all added functions which enables us sort and select our data accoring to preferences

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -4,11 +4,19 @@ const {
   getBootcamp,
   createBootcamp,
   updateBootcamp,
-  deleteBootcamp
+  deleteBootcamp,
+  getBootcampsInRadius
 } = require('../controllers/bootcamps');
+
+// Include other resource routes
+const courseRouter = require('./courses');
 
 const router = express.Router();
 
+//Re-route into other resource routes
+router.use('/:bootcampId/courses', courseRouter);
+
+router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 router.route('/').get(getBootcamps).post(createBootcamp);
 router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp);
 
