1b10098851c70daf7060c29c609b64d921fb4886
john katua <johnkatua99@gmail.com>
2020-08-04 02:33:51 +0300

    We created user model to help us authenticate our users and  we also installed jsonwebtoken to hash our password and bcrtypt to encrypt our password, we also created a middleware to protect our routes and token verification

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -5,19 +5,26 @@ const {
   createBootcamp,
   updateBootcamp,
   deleteBootcamp,
-  getBootcampsInRadius
+  getBootcampsInRadius,
+  bootcampPhotoUpload
 } = require('../controllers/bootcamps');
+
+const Bootcamp = require('../models/Bootcamp');
+const advancedResults = require('../middleware/advancedResults');
 
 // Include other resource routes
 const courseRouter = require('./courses');
 
 const router = express.Router();
 
+const { protect, authorize } = require('../middleware/auth');
+
 //Re-route into other resource routes
 router.use('/:bootcampId/courses', courseRouter);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
-router.route('/').get(getBootcamps).post(createBootcamp);
-router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp);
+router.route('/:id/photo').put(protect, authorize('publisher', 'admin'), bootcampPhotoUpload);
+router.route('/').get(advancedResults(Bootcamp, 'courses'), getBootcamps).post(protect, authorize('publisher', 'admin'), createBootcamp);
+router.route('/:id').get(getBootcamp).put(protect, authorize('publisher', 'admin'), updateBootcamp).delete(protect, authorize('publisher', 'admin'), deleteBootcamp);
 
 module.exports = router;
